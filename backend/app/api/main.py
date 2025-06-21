5b0bdaaea163c0d75fe4ae3d2a5b52ab34a9b122
Morgan <ngetichmorgan6@gmail.com>
2025-06-21 21:27:30 +0300

    Create the admin route

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
-from app.api.routes import users, login
+from app.api.routes import (
+  users,
+  login,
+)
 
+from app.api.routes import admin
 api_router = APIRouter()
 
 api_router.include_router(login.router, tags=["login"] )
+api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
