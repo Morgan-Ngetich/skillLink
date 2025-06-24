40e4da81c424925b3c6924fb987ba24309bd8aff
Morgan <ngetichmorgan6@gmail.com>
2025-06-24 14:27:28 +0300

    Render the  Toaster component for global notifications

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import themeSystem from './theme';
 
+import { Toaster } from './components/ui/toaster';
+
 const queryClient = new QueryClient();
 
 const router = createRouter({
@@ -21,6 +23,7 @@ createRoot(document.getElementById('root')!).render(
     <ChakraProvider value={themeSystem}>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <Toaster /> {/*This is to render the toaster gloablly */}
       </QueryClientProvider>
     </ChakraProvider>
   </StrictMode>
