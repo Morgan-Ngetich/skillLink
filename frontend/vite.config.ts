394a34047e617634f4554dc355a200e23e028c56
Morgan <ngetichmorgan6@gmail.com>
2025-06-29 03:19:13 +0300

    Remove VitePWA plugin configuration from vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,34 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 import path from 'node:path'
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['vite.svg'], 
-      manifest: {
-        name: 'SkillUP',
-        short_name: 'skillUp',
-        description: 'SkillUP-powered PWA',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/vite.svg',
-            sizes: '192x192',
-            type: 'image/svg+xml'
-          },
-          {
-            src: '/vite.svg',
-            sizes: '512x512',
-            type: 'image/svg+xml',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
   ],
   resolve: {
     alias: {
