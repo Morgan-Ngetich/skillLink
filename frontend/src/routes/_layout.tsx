71b491fa41944cb5c4e63bf628c02b7702114fcc
Morgan <ngetichmorgan6@gmail.com>
2025-07-08 09:08:13 +0300

    removed the Auth spinner logic. Its already handled in main.ts

--- frontend/src/routes/_layout.tsx
+++ frontend/src/routes/_layout.tsx
@@ -1,20 +1,11 @@
 // src/routes/_layout.tsx
 import { createFileRoute, Outlet, redirect } from '@tanstack/react-router';
-import { Flex, Spinner } from '@chakra-ui/react';
 import AppLayout from '../AppLayout';
 import { isLoggedIn } from "../hooks/auth/authState"
-import { useAuth } from "../hooks/auth/useAuth"
 function Layout() {
-  const { isLoading } = useAuth()
   return (
     <AppLayout>
-      {isLoading ? (
-        <Flex justify="center" align="center" height="100vh" width="full">
-          <Spinner size="xl" color="teal.500" />
-        </Flex>
-      ) : (
         <Outlet />
-      )}
     </AppLayout>
   );
 }
