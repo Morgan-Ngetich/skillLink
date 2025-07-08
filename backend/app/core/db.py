6035b862e143be404c4a2aafac27e5a0b495dce6
Morgan <ngetichmorgan6@gmail.com>
2025-07-08 05:15:21 +0300

    Refactor get_session function to specify return type as Generator

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -2,10 +2,10 @@ from sqlmodel import create_engine, Session, SQLModel, select
 from app.core.config import settings
 from app.models.users import User, RoleName  # Import all related models
 from app.core.security import get_password_hash
-from app import crud
+from typing import Generator
 
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, echo=True)
 
-def get_session():
+def get_session() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
