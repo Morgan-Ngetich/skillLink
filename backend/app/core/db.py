a061f821ed89183f86e797a69c53d59a848b9b22
Morgan <ngetichmorgan6@gmail.com>
2025-07-10 12:42:48 +0300

    Removed unused imports

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,7 +1,5 @@
 from sqlmodel import create_engine, Session, SQLModel, select
 from app.core.config import settings
-from app.models.users import User, RoleName  # Import all related models
-from app.core.security import get_password_hash
 from typing import Generator
 
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, echo=True)
