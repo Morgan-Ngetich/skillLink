ddc9ee81ff5db84e241a8eac750ee55372cdfb20
Morgan <ngetichmorgan6@gmail.com>
2025-07-13 23:30:19 +0300

    Remove hasSpecialChar condition.

--- frontend/src/utils/validator.ts
+++ frontend/src/utils/validator.ts
@@ -6,14 +6,13 @@ export const isValidEmail = (email: string): boolean => {
 export const hasUpperCase = (password: string) => /[A-Z]/.test(password);
 export const hasLowerCase = (password: string) => /[a-z]/.test(password);
 export const hasNumber = (password: string) => /[0-9]/.test(password);
-export const hasSpecialChar = (password: string) => /[^A-Za-z0-9]/.test(password);
+// export const hasSpecialChar = (password: string) => /[^A-Za-z0-9]/.test(password);
 
 export const isValidPassword = (password: string): boolean => {
   return (
     password.length >= 8 &&
     hasUpperCase(password) &&
     hasLowerCase(password) &&
-    hasNumber(password) &&
-    hasSpecialChar(password)
+    hasNumber(password)
   );
 };
