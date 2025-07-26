f8e4cd281c8cfd07646007c34006d567aea395c9
Morgan <ngetichmorgan6@gmail.com>
2025-07-26 21:55:43 +0300

    Delete 'frontend/scripts/block-nginx.sh' file that prevented its commit.

--- frontend/scripts/block-nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Prevent committing nginx.conf from frontend directory
-
-if git diff --cached --name-only | grep -q "^frontend/nginx.conf$"; then
-  echo "🚫 Commit rejected: frontend/nginx.conf should not be committed."
-  exit 1
-fi
