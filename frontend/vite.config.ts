70afe7dce312aab2017e8f7d5724daaeb4947676
Morgan <ngetichmorgan6@gmail.com>
2025-08-23 09:03:57 +0300

    add `mdx` within vite's plugin.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // import { VitePWA } from 'vite-plugin-pwa'
 import path from 'node:path'
+import mdx from "@mdx-js/rollup"
 
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [react()],
+    plugins: [
+      mdx({
+        providerImportSource: "@mdx-js/react", // makes MDX respect <MDXProvider />
+      }),
+      react(),
+    ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
