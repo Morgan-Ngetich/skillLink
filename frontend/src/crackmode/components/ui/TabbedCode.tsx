bf88aa6d8d62c9af55514769ec4941af016fbc00
Morgan <ngetichmorgan6@gmail.com>
2025-08-25 16:02:18 +0300

    Update the ux. - add a `copy-indicator` button - allow `X-overflow` within the codeblock content.

--- frontend/src/crackmode/components/ui/TabbedCode.tsx
+++ frontend/src/crackmode/components/ui/TabbedCode.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Tabs } from "@chakra-ui/react"
+import { Tabs, IconButton } from "@chakra-ui/react"
 import { CodeBlock } from "@chakra-ui/react"
 import { shikiAdapter } from "@/crackmode/hooks/shikiAdapter"
 
@@ -38,22 +38,26 @@ export const TabbedCode = ({ files }: TabbedCodeProps) => {
 
       {files.map((file, idx) => (
         <Tabs.Content key={idx} value={file.title}>
-            <CodeBlock.AdapterProvider value={shikiAdapter}>
-              <CodeBlock.Root code={file.code.trim()} language={file.language}>
-                <CodeBlock.Header>
-                  <CodeBlock.Title>{file.language}</CodeBlock.Title>
-                  <CodeBlock.Control>
-                    <CodeBlock.CopyTrigger />
-                    <CodeBlock.CollapseTrigger />
-                  </CodeBlock.Control>
-                </CodeBlock.Header>
-                <CodeBlock.Content>
-                  <CodeBlock.Code>
-                    <CodeBlock.CodeText />
-                  </CodeBlock.Code>
-                </CodeBlock.Content>
-              </CodeBlock.Root>
-            </CodeBlock.AdapterProvider>
+          <CodeBlock.AdapterProvider value={shikiAdapter}>
+            <CodeBlock.Root code={file.code.trim()} language={file.language}>
+              <CodeBlock.Header>
+                <CodeBlock.Title>{file.language}</CodeBlock.Title>
+                <CodeBlock.Control>
+                  <CodeBlock.CopyTrigger asChild>
+                    <IconButton variant="ghost" size="2xs">
+                      <CodeBlock.CopyIndicator />
+                    </IconButton>
+                  </CodeBlock.CopyTrigger>
+                  <CodeBlock.CollapseTrigger />
+                </CodeBlock.Control>
+              </CodeBlock.Header>
+              <CodeBlock.Content w="full" overflowX={'auto'}>
+                <CodeBlock.Code>
+                  <CodeBlock.CodeText />
+                </CodeBlock.Code>
+              </CodeBlock.Content>
+            </CodeBlock.Root>
+          </CodeBlock.AdapterProvider>
         </Tabs.Content>
       ))}
     </Tabs.Root>
