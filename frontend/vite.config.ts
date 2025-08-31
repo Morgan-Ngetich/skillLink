3cad6e29ea292a6cc2365603e1ade9e831f3c848
Morgan <ngetichmorgan6@gmail.com>
2025-08-31 17:13:24 +0300

    Use `Terser Options (Built-in with Vite)` to remove `console.log`. This won’t affect `console.error` or `console.warn` by default.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,17 +17,27 @@ export default defineConfig(({ mode }) => {
         '@': path.resolve(__dirname, './src'),
       },
     },
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          // This won’t affect console.error or console.warn by default. It only drops log, info, debug, etc.
+          drop_console: true,  // Removes console.log/debug/info
+          drop_debugger: true  // Removes debugger statements
+        }
+      }
+    },
     server: mode === 'development'
       ? {
-          port: 5174,
-          proxy: {
-            '/api': {
-              target: 'http://localhost:8000',
-              changeOrigin: true,
-              rewrite: (path) => path,
-            },
+        port: 5174,
+        proxy: {
+          '/api': {
+            target: 'http://localhost:8000',
+            changeOrigin: true,
+            rewrite: (path) => path,
           },
-        }
+        },
+      }
       : undefined,
   };
 });
