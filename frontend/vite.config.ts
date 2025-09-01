15576ea41bd705996f8f5b2ffac50836922947e0
Morgan <ngetichmorgan6@gmail.com>
2025-09-01 17:32:40 +0300

    refactor Vite config for improved SSR handling.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type UserConfig, type ConfigEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 // import { VitePWA } from 'vite-plugin-pwa'
 import path from 'node:path'
 import mdx from "@mdx-js/rollup"
 
-export default defineConfig(({ mode }) => {
+export default defineConfig((configEnv: ConfigEnv): UserConfig => {
+  const { mode } = configEnv;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const ssrBuild = (configEnv as any).ssrBuild
+
   return {
     plugins: [
       mdx({
@@ -18,14 +22,23 @@ export default defineConfig(({ mode }) => {
       },
     },
     build: {
-      minify: 'terser',
-      terserOptions: {
+      minify: mode === 'production' ? 'terser' : false,
+      terserOptions: mode === 'production' ? {
         compress: {
           // This won’t affect console.error or console.warn by default. It only drops log, info, debug, etc.
           drop_console: true,  // Removes console.log/debug/info
           drop_debugger: true  // Removes debugger statements
         }
-      }
+      } : undefined,
+
+      rollupOptions: ssrBuild ? {
+        input: '/src/crackmode/components/seo/entry-server',
+        external: ['fs', 'path', 'url']
+      } : undefined
+    },
+    ssr: {
+      // Don't externalize these for SSR
+      noExternal: ['@tanstack/react-router', '@chakra-ui/react', 'react-helmet-async']
     },
     server: mode === 'development'
       ? {
