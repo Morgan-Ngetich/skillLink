d4d53b29a85ca6d8f847eeda4d4a299efc686a9b
Morgan <ngetichmorgan6@gmail.com>
2025-09-05 16:31:23 +0300

    add `lineClamp={1}` to allow overflow and prevent text Wrap.

--- frontend/src/components/ui/tooltip.tsx
+++ frontend/src/components/ui/tooltip.tsx
@@ -30,7 +30,7 @@ export const Tooltip = React.forwardRef<HTMLDivElement, TooltipProps>(
         <ChakraTooltip.Trigger asChild>{children}</ChakraTooltip.Trigger>
         <Portal disabled={!portalled} container={portalRef}>
           <ChakraTooltip.Positioner>
-            <ChakraTooltip.Content ref={ref} {...contentProps}>
+            <ChakraTooltip.Content ref={ref} {...contentProps} lineClamp={1}>
               {showArrow && (
                 <ChakraTooltip.Arrow>
                   <ChakraTooltip.ArrowTip />
