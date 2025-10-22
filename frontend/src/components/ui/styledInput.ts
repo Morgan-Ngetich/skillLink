1ddf345f00190646e55d65eb1e1eb4368d0955ab
Morgan <ngetichmorgan6@gmail.com>
2025-10-22 16:48:55 +0300

    add `StyledTextarea` for textarea inputs.

--- frontend/src/components/ui/styledInput.ts
+++ frontend/src/components/ui/styledInput.ts
@@ -1,6 +1,13 @@
-"use client";
-
 import { chakra } from "@chakra-ui/react";
 import { inputRecipe } from "@/recipes/input.recipe";
 
 export const StyledInput = chakra("input", inputRecipe);
+
+
+export const StyledTextarea = chakra("textarea", {
+  base: {
+    ...inputRecipe.base,
+    minH: "100px", // make it taller by default for typing
+    resize: "vertical", // allow resizing
+  },
+});
