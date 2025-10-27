7f31a5c30559512362550681687d845e0b639c61
Morgan <ngetichmorgan6@gmail.com>
2025-10-27 12:41:19 +0300

    rename 'tag' from og to crackmode.

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -12,7 +12,7 @@ from app.api.routes import admin
 api_router = APIRouter()
 
 api_router.include_router(login.router, tags=["login"])
-api_router.include_router(og.router, tags=["og"])
+api_router.include_router(og.router, tags=["crackmode"])
 api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(profile.router, prefix="/profile", tags=["profile"])
