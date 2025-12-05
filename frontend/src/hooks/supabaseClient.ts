287d6716be4e2126b15470c2134a82b93122176a
Morgan <ngetichmorgan6@gmail.com>
2025-12-06 01:25:52 +0300

    removed unwanted files and folders.

--- frontend/src/hooks/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-console.log("supabaseUrl", supabaseUrl)
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
