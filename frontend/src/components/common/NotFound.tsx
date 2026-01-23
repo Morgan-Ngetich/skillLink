8e18b85a34f4cd9ea4969ef89abb2386fc325ce0
Morgan <ngetichmorgan6@gmail.com>
2026-01-23 23:44:23 +0300

    Remove crackmode fallback logic.

--- frontend/src/components/common/NotFound.tsx
+++ frontend/src/components/common/NotFound.tsx
@@ -1,12 +1,6 @@
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
-import { useLocation } from "@tanstack/react-router";
 
 const NotFound = () => {
-  const location = useLocation();
-
-  // Check if the current path starts with /crackmode/
-  const isCrackMode = location.pathname.startsWith('/crackmode');
-
   return (
     <Flex
       justify="center"
@@ -16,25 +10,16 @@ const NotFound = () => {
       textAlign="center"
     >
       <Box>
-        {isCrackMode ? (
-          <>
-            <Text fontSize="4xl" fontWeight="bold" mb={2} color={"orange"}>
-              Coming Soon!
-            </Text>
-            <Text fontSize="md" mb={6}>
-              This page is under development and will be available soon.
-            </Text>
-          </>
-        ) : (
-          <>
-            <Text fontSize="4xl" fontWeight="bold" mb={2}>
-              Oops! Page not found.
-            </Text>
-            <Text fontSize="md" mb={6}>
-              The page you're looking for doesn't exist or has been moved.
-            </Text>
-          </>
-        )}
+        (
+        <>
+          <Text fontSize="4xl" fontWeight="bold" mb={2}>
+            Oops! Page not found.
+          </Text>
+          <Text fontSize="md" mb={6}>
+            The page you're looking for doesn't exist or has been moved.
+          </Text>
+        </>
+        )
 
         <Button
           onClick={() => history.back()}
