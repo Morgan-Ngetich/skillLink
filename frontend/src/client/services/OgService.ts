ea36f511ef348337d0c3cca4d831c6cdf155ac54
Morgan <ngetichmorgan6@gmail.com>
2026-02-23 18:57:01 +0300

    Add the og - image generation- services.

---
+++ frontend/src/client/services/OgService.ts
@@ -0,0 +1,30 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { CancelablePromise } from '../core/CancelablePromise';
+import { OpenAPI } from '../core/OpenAPI';
+import { request as __request } from '../core/request';
+export class OgService {
+    /**
+     * Get Profile Og
+     * @returns any Successful Response
+     * @throws ApiError
+     */
+    public static getProfileOgApiV1ProfilesOgProfileUuidGet({
+        uuid,
+    }: {
+        uuid: string,
+    }): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/api/v1/profiles/og/profile/{uuid}',
+            path: {
+                'uuid': uuid,
+            },
+            errors: {
+                422: `Validation Error`,
+            },
+        });
+    }
+}
--- frontend/src/client/services/ProfilesService.ts
+++ frontend/src/client/services/ProfilesService.ts
@@ -123,4 +123,25 @@ export class ProfilesService {
             },
         });
     }
+    /**
+     * Get Profile Og
+     * @returns any Successful Response
+     * @throws ApiError
+     */
+    public static getProfileOgApiV1ProfilesOgProfileUuidGet({
+        uuid,
+    }: {
+        uuid: string,
+    }): CancelablePromise<any> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/api/v1/profiles/og/profile/{uuid}',
+            path: {
+                'uuid': uuid,
+            },
+            errors: {
+                422: `Validation Error`,
+            },
+        });
+    }
 }
